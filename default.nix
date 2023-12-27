b03f44ceeb6d8bd1376f7c6928553001cb38a99d
John Shaffer <john@insilica.co>
2023-12-27 13:26:17 -0600

    Use dev-shell

--- default.nix
+++
@@ -1,6 +0,0 @@
-{ sources ? import ./nix/sources.nix, pkgs ? import sources.nixpkgs { } }:
-with pkgs;
-let jdk = pkgs.openjdk17;
-in mkShell {
-  buildInputs = [ (clojure.override { jdk = jdk; }) csv2parquet jdk rlwrap ];
-}
